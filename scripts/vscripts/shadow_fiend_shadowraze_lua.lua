3fe0d479c55dfa94a164b110ef244335c61ff46d
Vadym Kopyko <vadimivkop@gmail.com>
2023-09-21 18:50:00 +0300

    Fix talent naming value for shadowraze

--- scripts/vscripts/shadow_fiend_shadowraze_lua.lua
+++ scripts/vscripts/shadow_fiend_shadowraze_lua.lua
@@ -45,12 +45,12 @@ function shadowraze.OnSpellStart( this )
 	local dmg_type = DAMAGE_TYPE_MAGICAL
 
 	-- Talents
-	local is_Talent_15_L = this:GetCaster():FindAbilityByName("special_bonus_yarik_coils_dmg"):GetLevel() -- +150
+	local is_Talent_15_R = this:GetCaster():FindAbilityByName("special_bonus_yarik_coils_dmg"):GetLevel() -- +150
 	-- local is_Talent_15_R +50 attack dmg 
 	-- local is_Talent_20_L 20% bash for 2 sec
-	local is_Talent_20_R = this:GetCaster():FindAbilityByName("special_bonus_yarik_stack_dmg"):GetLevel() -- + 130
-	local is_Talent_25_L = this:GetCaster():FindAbilityByName("special_bonus_yarik_attack_dmg_to_coils"):GetLevel()
-	local is_Talent_25_R = this:GetCaster():FindAbilityByName("special_bonus_yarik_pure_coils"):GetLevel()
+	local is_Talent_20_L = this:GetCaster():FindAbilityByName("special_bonus_yarik_stack_dmg"):GetLevel() -- + 130
+	local is_Talent_25_R = this:GetCaster():FindAbilityByName("special_bonus_yarik_attack_dmg_to_coils"):GetLevel()
+	local is_Talent_25_L = this:GetCaster():FindAbilityByName("special_bonus_yarik_pure_coils"):GetLevel()
 
 	if is_Talent_15_L > 0 then
 		base_damage = base_damage + 150
